00546f186fdd8cad69a588161623b25aaf84abf4
Annie Chiu <77923093+annieycchiu@users.noreply.github.com>
2023-04-29 20:20:34 -0700

    v1

---
+++ app.py
@@ -0,0 +1,29 @@
+import torch
+from PIL import Image
+import requests
+import os
+import shutil
+import subprocess
+
+import streamlit as st
+from streamlit_lottie import st_lottie
+
+st.write('Hello world!')
+cwd = os.getcwd()
+st.write(cwd)
+
+
+image_file = st.file_uploader("Please upload a food image",type=['png','jpeg','jpg'])
+
+if image_file is not None:
+    file_details = {"FileName": image_file.name,"FileType": image_file.type}
+
+    st.write(file_details)
+
+    img = Image.open(image_file)
+    st.write('Original image')
+    st.image(img, use_column_width=False)
+
+    with open(os.path.join('user_uploads', image_file.name), "wb") as f: 
+        f.write(image_file.getbuffer())         
+        st.success("Saved File")
