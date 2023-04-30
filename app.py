5d91f7e2fa81e254163ffb91717cc971865e78aa
annieycchiu <annieycchiu@gmail.com>
2023-04-30 01:18:31 -0700

    v6

--- app.py
+++ app.py
@@ -1,54 +1,19 @@
-import torch
-from PIL import Image
-import requests
-import os
-import shutil
-import subprocess
-
 import streamlit as st
-from streamlit_lottie import st_lottie
 from deta import Deta
-
-
-# Data to be written to Deta Base
-with st.form("form"):
-    name = st.text_input("Your name")
-    age = st.number_input("Your age")
-    submitted = st.form_submit_button("Store in database")
+from PIL import Image
 
 
 # Connect to Deta Base with your Data Key
-deta = Deta(st.secrets['DETA_KEY'])
+deta = Deta(st.secrets["DETA_KEY"])
 
-# Create a new database "example-db"
-# If you need a new database, just use another name.
-db = deta.Base("recipes")
+db = deta.Drive("user-images")
+image_file = st.file_uploader("Please upload a food image",type=['png','jpeg','jpg'])
 
-# If the user clicked the submit button,
-# write the data from the form to the database.
-# You can store any data you want here. Just modify that dictionary below (the entries between the {}).
-if submitted:
-    db.put({"name": name, "age": age})
+if image_file is not None:
+    db.put(image_file.name, image_file)
+    st.success('Successfully saved image')
 
-"---"
-"Here's everything stored in the database:"
-# This reads all items from the database and displays them to your app.
-# db_content is a list of dictionaries. You can do everything you want with it.
-db_content = db.fetch().items
-st.write(db_content)
+    img_ = db.get(image_file.name)
 
-# # Connect to Deta Base with your Data Key
-# deta = Deta(st.secrets['DETA_KEY'])
-
-# image_file = st.file_uploader("Upload a food image", type=['png','jpeg','jpg'])
-
-# if image_file is not None:
-#     file_details = {"FileName": image_file.name,"FileType": image_file.type}
-
-#     # Save user uploaded image to Deta database
-#     db.put(image_file.name, image_file)
-#     st.success('Image saved successfully')
-
-#     # Retrieve image from Deta database
-#     db.get(image_file.name)
-#     st.success('Image retrieved successfully')
+    img = Image.open(img_)
+    st.image(img, use_column_width=False)
