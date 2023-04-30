6626b4d7baf39d013fb646c663d557187ce23710
annieycchiu <annieycchiu@gmail.com>
2023-04-29 23:11:16 -0700

    add database

--- database.py
+++ database.py
@@ -1,10 +1,6 @@
 import os
-
 from deta import Deta
-from dotenv import load_dotenv
 
-DETA_KEY = 'b0mwg57zwdt_cwbFheLqscgazkCnk9AcX6Kfn3eTNYBW'
-
-deta = Deta(DETA_KEY)
+deta = Deta(st.secrets['DETA_KEY'])
 
 db = deta.drive('user-images')
