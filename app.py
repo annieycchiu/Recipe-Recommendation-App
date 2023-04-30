3551e1ae2e10656c3bc7f2a2c792c0803ccdc541
annieycchiu <annieycchiu@gmail.com>
2023-04-30 01:26:43 -0700

    v6

--- app.py
+++ app.py
@@ -7,13 +7,20 @@ from PIL import Image
 deta = Deta(st.secrets["DETA_KEY"])
 
 db = deta.Drive("user-images")
+
+col1, col2 = st.columns(2)
 image_file = st.file_uploader("Please upload a food image",type=['png','jpeg','jpg'])
 
 if image_file is not None:
     db.put(image_file.name, image_file)
     st.success('Successfully saved image')
 
-    img_ = db.get(image_file.name)
+    with col1:
+        uploaded_img_path = db.get(image_file.name)
+        uploaded_img = Image.open(uploaded_img_path)
 
-    img = Image.open(img_)
-    st.image(img, use_column_width=False)
+        st.write('Original image')
+        st.image(uploaded_img, use_column_width=True)
+
+    with col2:
+        st.write('Inferenced image')
