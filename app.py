af0b8d19cb23cd5cf8a1c0fa59d0237af3fe12d5
annieycchiu <annieycchiu@gmail.com>
2023-04-30 15:44:32 -0700

    v7

--- app.py
+++ app.py
@@ -8,32 +8,53 @@ import subprocess
 
 import streamlit as st
 from streamlit_lottie import st_lottie
-from deta import Deta
+#from deta import Deta
+import s3fs
 
 
-# Connect to Deta Base with your Data Key
-deta = Deta(st.secrets["DETA_KEY"])
 
-db = deta.Drive("user-images")
+# Create connection object.
+# `anon=False` means not anonymous, i.e. it uses access keys to pull data.
+fs = s3fs.S3FileSystem(anon=False)
 
-col1, col2 = st.columns(2)
-image_file = st.file_uploader("Please upload a food image",type=['png','jpeg','jpg'])
+# Retrieve file contents.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def read_file(filename):
+    with fs.open(filename) as f:
+        return f.read().decode("utf-8")
 
-if image_file is not None:
-    db.put(image_file.name, image_file)
-    st.success('Successfully saved image')
+content = read_file("testbucket-jrieke/myfile.csv")
 
-    with col1:
-        st.write('Original image')
-        uploaded_img_path = db.get(image_file.name)
-        uploaded_img = Image.open(uploaded_img_path)
-        st.image(uploaded_img, use_column_width=True)
+# Print results.
+for line in content.strip().split("\n"):
+    name, pet = line.split(",")
+    st.write(f"{name} has a :{pet}:")
 
-    with col2:
-        st.write('Inferenced image')
-        # if device == 'cuda':
-        #     detect(weights="models/best.pt", source=uploaded_img_path, save_txt=True, device=0) 
-        # else: 
-        #     detect(weights="models/best.pt", source=uploaded_img_path, save_txt=True, device='cpu')
+
+# # Connect to Deta Base with your Data Key
+# deta = Deta(st.secrets["DETA_KEY"])
+
+# db = deta.Drive("user-images")
+
+# col1, col2 = st.columns(2)
+# image_file = st.file_uploader("Please upload a food image",type=['png','jpeg','jpg'])
+
+# if image_file is not None:
+#     db.put(image_file.name, image_file)
+#     st.success('Successfully saved image')
+
+#     with col1:
+#         st.write('Original image')
+#         uploaded_img_path = db.get(image_file.name)
+#         uploaded_img = Image.open(uploaded_img_path)
+#         st.image(uploaded_img, use_column_width=True)
+
+#     with col2:
+#         st.write('Inferenced image')
+#         # if device == 'cuda':
+#         #     detect(weights="models/best.pt", source=uploaded_img_path, save_txt=True, device=0) 
+#         # else: 
+#         #     detect(weights="models/best.pt", source=uploaded_img_path, save_txt=True, device='cpu')
 
         
