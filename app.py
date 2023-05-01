24b2b2a5e3b9d63a7ed8665b2a269de982988f5e
annieycchiu <annieycchiu@gmail.com>
2023-04-30 22:13:00 -0700

    running version

--- app.py
+++ app.py
@@ -4,57 +4,114 @@ import requests
 import os
 import shutil
 import subprocess
-# import detect
+import random
+import detect
 
 import streamlit as st
 from streamlit_lottie import st_lottie
-#from deta import Deta
-import s3fs
+from streamlit_option_menu import option_menu
+from allrecipes import AllRecipes
+
+cwd = os.getcwd()
+
+banner_img = Image.open('statics/food-banner.png')
+st.image(banner_img, use_column_width=True)
 
 
-
-# Create connection object.
-# `anon=False` means not anonymous, i.e. it uses access keys to pull data.
-fs = s3fs.S3FileSystem(anon=False)
-
-# Retrieve file contents.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def read_file(filename):
-    with fs.open(filename) as f:
-        return f.read().decode("utf-8")
-
-content = read_file("streamlit-food-images/test.csv")
-
-# Print results.
-for line in content.strip().split("\n"):
-    name, pet = line.split(",")
-    st.write(f"{name} has a :{pet}:")
+selected = option_menu(
+    menu_title=None,
+    options=["What's on your plate?", "Recipe Recommendations"],
+    icons=['cloud-upload', 'bookmark-heart'],
+    default_index=0,
+    orientation='horizontal'
+    )
 
 
-# # Connect to Deta Base with your Data Key
-# deta = Deta(st.secrets["DETA_KEY"])
+with open('data_food_robo.yaml') as f:
+    data = f.readlines()
+    categories = data[-1]
+    categories = categories.strip('names: [').strip(']')
 
-# db = deta.Drive("user-images")
+cat_list = [cat[1:-1] for cat in categories.split(', ')]
 
-# col1, col2 = st.columns(2)
-# image_file = st.file_uploader("Please upload a food image",type=['png','jpeg','jpg'])
 
-# if image_file is not None:
-#     db.put(image_file.name, image_file)
-#     st.success('Successfully saved image')
+if selected == "What's on your plate?":
+    col1, col2 = st.columns(2)
+    image_file = st.file_uploader('',type=['png', 'jpeg', 'jpg'])
 
-#     with col1:
-#         st.write('Original image')
-#         uploaded_img_path = db.get(image_file.name)
-#         uploaded_img = Image.open(uploaded_img_path)
-#         st.image(uploaded_img, use_column_width=True)
+    if image_file is not None:
 
-#     with col2:
-#         st.write('Inferenced image')
-#         # if device == 'cuda':
-#         #     detect(weights="models/best.pt", source=uploaded_img_path, save_txt=True, device=0) 
-#         # else: 
-#         #     detect(weights="models/best.pt", source=uploaded_img_path, save_txt=True, device='cpu')
+        with col1:
+            st.write('Original image')
+            original_img = Image.open(image_file)
+            st.image(original_img, use_column_width=True)
 
-        
+            with open(os.path.join(f'{cwd}/user_uploads', image_file.name), "wb") as f: 
+                f.write(image_file.getbuffer())    
+                st.success("Saved File")
+
+        with col2:
+            st.write('Inferenced image')
+
+            # Check Folder is exists or Not
+            folderPath = f'{cwd}/inferenced_imgs'
+            if os.path.exists(folderPath):    
+                # Delete Folder code
+                shutil.rmtree(folderPath)
+
+            
+            detect.run(source='user_uploads', 
+                       weights='best.pt', 
+                       project=f'{cwd}', 
+                       name='inferenced_imgs', 
+                       save_txt=True)
+            
+            inferred_img = os.path.join('inferenced_imgs/', image_file.name)
+            st.image(inferred_img, use_column_width=True)
+
+
+elif selected == "Recipe Recommendations":
+    image_file_name = 'pizza_salad'
+    labels_path = f'inferenced_imgs/labels/{image_file_name}.txt'
+    with open(labels_path) as f:
+        lines = f.readlines()
+        labels = set()
+        for line in lines:
+            labels.add(line.split(' ')[0])
+
+    labels = list(labels)
+    labels_w = []
+    for num in labels:
+        labels_w.append(cat_list[int(num)])
+
+
+    for ingre in labels_w:
+        search_string = ingre
+        query_result = AllRecipes.search(search_string)
+        num = random.randint(0, len(query_result))
+        recipe = query_result[num]
+
+        name = recipe['name']
+        url = recipe['url']
+        rate = recipe['rate']
+        image = recipe['image']
+
+        detailed_recipe = AllRecipes.get(url) 
+        servings = detailed_recipe['nb_servings']
+        ingres = detailed_recipe['ingredients']
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.subheader(name)
+            st.image(image, use_column_width=True)
+            st.write(f'Rating: {rate}')
+            st.write(f'[More details >]({url})')
+        with col2:
+            st.subheader('')
+            st.write(f'Servings: {servings}')
+            st.write('Ingredients:')
+            for ingre in ingres:
+                st.write(ingre)
+
+        st.write('---')
