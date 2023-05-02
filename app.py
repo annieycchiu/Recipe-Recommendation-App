197107ca7888b40d850a5e03af8f3f7d038c3d9f
annieycchiu <annieycchiu@gmail.com>
2023-05-02 15:24:24 -0700

    update dispaly

--- app.py
+++ app.py
@@ -52,12 +52,12 @@ if selected == "What's on your plate":
         img_array = np.array(image)
 
         with col1:
-            st.write('Original image')
+            st.write('Original image :camera:')
             original_img = Image.open(image_file)
             st.image(original_img, use_column_width=True)
 
         with col2:
-            st.write('Inferenced image')
+            st.write('Inferenced image :hammer_and_wrench:')
 
             inferred_img, inferred_label, _ = detect.run(
                 objs=[ cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR) ],
@@ -72,37 +72,40 @@ if selected == "What's on your plate":
 
 
 if selected == "Recipe Recommendations":
-    inferred_label = st.session_state.inferred_label
+    if inferred_label is None:
+        st.write('Please upload an image for detection first! :camera_with_flash:')
+    else: 
+        inferred_label = st.session_state.inferred_label
 
-    labels = list(set(inferred_label))
+        labels = list(set(inferred_label))
 
-    for ingre in labels:
-        search_string = ingre
-        query_result = AllRecipes.search(search_string)
-        num = random.randint(0, len(query_result)-1)
-        recipe = query_result[num]
+        for ingre in labels:
+            search_string = ingre
+            query_result = AllRecipes.search(search_string)
+            num = random.randint(0, len(query_result)-1)
+            recipe = query_result[num]
 
-        name = recipe['name']
-        url = recipe['url']
-        rate = recipe['rate']
-        image = recipe['image']
+            name = recipe['name']
+            url = recipe['url']
+            rate = recipe['rate']
+            image = recipe['image']
 
-        detailed_recipe = AllRecipes.get(url) 
-        servings = detailed_recipe['nb_servings']
-        ingres = detailed_recipe['ingredients']
+            detailed_recipe = AllRecipes.get(url) 
+            servings = detailed_recipe['nb_servings']
+            ingres = detailed_recipe['ingredients']
 
-        col1, col2 = st.columns(2)
+            col1, col2 = st.columns(2)
 
-        with col1:
-            st.subheader(name)
-            st.image(image, use_column_width=True)
-            st.write(f'Rating: {rate}')
-            st.write(f'[More details >]({url})')
-        with col2:
-            st.subheader('')
-            st.write(f'Servings: {servings}')
-            st.write('Ingredients:')
-            for ingre in ingres:
-                st.write(ingre)
+            with col1:
+                st.subheader(name)
+                st.image(image, use_column_width=True)
+                st.write(f'Rating: {rate}')
+                st.write(f'[More details >]({url})')
+            with col2:
+                st.subheader('')
+                st.write(f'Servings: {servings}')
+                st.write('Ingredients:')
+                for ingre in ingres:
+                    st.write(ingre)
 
-        st.write('---')
+            st.write('---')
